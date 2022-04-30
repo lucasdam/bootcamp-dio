01c15d7722485867b23cabfc9743a55a618253b4
Lucas Damasceno <54273070+lucasdam@users.noreply.github.com>
2022-04-30 20:05:05 -0300

    Adicionado o exercício de Módulos

---
+++ modulos/README.md
@@ -0,0 +1,3 @@
+# Módulos
+
+Este repositório contém a atividade prática do Curso "Módulos", que faz parte do Basecamp de Javascript da [Digital Innovation One](https://digitalinnovation.one/) ministrado por @stebsnusch.
---
+++ modulos/atividade/main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Lucas', 20));
+
+console.log(mostraCidade('Luna', 'São Paulo'));
+
+console.log(mostraHobby('Laura', 'Ouvir música'));
